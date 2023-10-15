df7e674f348af2319edf0826c2a2a829796f8c5d
echung32 <143231894+echung32@users.noreply.github.com>
2023-10-14 17:30:29 -1000

    Section 1B

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import {Hello} from './Hello.jsx';
+import {Info} from './Info.jsx';
+import {Task} from "./Task";
+
+const tasks = [
+    {_id: 1, text: 'First Task'},
+    {_id: 2, text: 'Second Task'},
+    {_id: 3, text: 'Third Task'},
+];
 
 export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-      <h1>i am a meteor</h1>
-    <Hello/>
-    <Info/>
-  </div>
+    <div>
+        <h1>Welcome to Meteor!</h1>
+        <ul>
+            {tasks.map(task => <Task key={task._id} task={task.text}/>)}
+        </ul>
+    </div>
 );
